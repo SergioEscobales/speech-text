dd270f50926f798156b2f059c503e167a894cf48
Sergio Escobales <sergio.escobales@straumann.com>
2022-05-04 17:34:22 +0200

    Primeros componentes

---
+++ src/icon-boop.jsx
@@ -0,0 +1,13 @@
+import MicNoneIcon from '@mui/icons-material/MicNone';
+
+function SomeComponent() {
+  return (
+    <div>
+        <button className='rounded'>
+            <MicNoneIcon icon="caret-down"  className='micColor'/>
+        </button>
+  </div>  
+  )
+}
+
+export default SomeComponent;
